8560cab083a60f9ce7f26b422135e6aadc5ae462
Raşit <22110207017@ogrenci.bartin.edu.tr>
2025-06-11 23:00:09 +0300

    Update Frmgrafik.cs

--- Frmgrafik.cs
+++ Frmgrafik.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 namespace personel_kayit_programi
 {
@@ -19,28 +12,58 @@ namespace personel_kayit_programi
             InitializeComponent();
         }
         SqlConnection baglanti = new SqlConnection("Data Source=RASIT\\SQLEXPRESS;Initial Catalog=PersonelVeriTabani;Integrated Security=True;Encrypt=False;TrustServerCertificate=True");
+
         private void Frmgrafik_Load(object sender, EventArgs e)
         {
-            //Şehir Grafiği
-            baglanti.Open();
-            SqlCommand komutg1 = new SqlCommand("select persehir,count(*) from tbl_personel group by persehir", baglanti);
-            SqlDataReader g1 = komutg1.ExecuteReader();
-            while (g1.Read())
+            // --- Şehir-Personel Sayısı Grafiği ---
+            try
             {
-                chart1.Series["sehirler"].Points.AddXY(g1[0], g1[1]);
-
+                baglanti.Open();
+                // SQL metni yerine Stored Procedure adını çağırıyoruz.
+                SqlCommand komutg1 = new SqlCommand("sp_GrafikSehirPersonelSayisi", baglanti);
+                komutg1.CommandType = CommandType.StoredProcedure; // Komut tipini belirtiyoruz.
+                SqlDataReader dr1 = komutg1.ExecuteReader();
+                while (dr1.Read())
+                {
+                    // Stored procedure'den gelen verileri grafiğe ekliyoruz.
+                    chart1.Series["sehirler"].Points.AddXY(dr1[0], dr1[1]);
+                }
             }
-            baglanti.Close();
-
-            //Meslek-Maaş Ortalaması Grafiği
-            baglanti.Open();
-            SqlCommand komutg2 = new SqlCommand("select permeslek,avg(permaaş) from tbl_personel group by permeslek", baglanti);
-            SqlDataReader g2 = komutg2.ExecuteReader();
-            while (g2.Read())
+            catch (Exception ex)
             {
-                chart2.Series["meslek-maas"].Points.AddXY(g2[0], g2[1]);
+                MessageBox.Show("Şehir grafiği yüklenirken hata oluştu: " + ex.Message);
+            }
+            finally
+            {
+                // Bağlantıyı her zaman kapatıyoruz.
+                if (baglanti.State == ConnectionState.Open)
+                    baglanti.Close();
             }
 
+            // --- Meslek-Maaş Ortalaması Grafiği ---
+            try
+            {
+                baglanti.Open();
+                // SQL metni yerine Stored Procedure adını çağırıyoruz.
+                SqlCommand komutg2 = new SqlCommand("sp_GrafikMeslekOrtalamaMaas", baglanti);
+                komutg2.CommandType = CommandType.StoredProcedure; // Komut tipini belirtiyoruz.
+                SqlDataReader dr2 = komutg2.ExecuteReader();
+                while (dr2.Read())
+                {
+                    // Stored procedure'den gelen verileri grafiğe ekliyoruz.
+                    chart2.Series["meslek-maas"].Points.AddXY(dr2[0], dr2[1]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Meslek-Maaş grafiği yüklenirken hata oluştu: " + ex.Message);
+            }
+            finally
+            {
+                // Bağlantıyı her zaman kapatıyoruz.
+                if (baglanti.State == ConnectionState.Open)
+                    baglanti.Close();
+            }
         }
     }
 }
